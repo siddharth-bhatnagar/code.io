60532685ea2be72426106c47b4d38801ed668ee9
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-06-27 19:11:39 +0530

    Local Storage

--- client/app.js
+++ client/app.js
@@ -1,6 +1,3 @@
-
-
-
 // Buttons
 const runBtn = $("#run");
 const resetBtn = $("#reset");
@@ -13,6 +10,52 @@ let defaultTheme = 'terminal';
 let defaultLang = 'javascript';
 let defaultFontSize = '18px';
 let defaultCode = 'console.log("Hello, World");';
+
+let langs = {
+    'Node JS': 'javascript',
+    'C++': 'c_cpp',
+    'Python': 'python',
+    'Java': 'java'
+};
+
+let themes = {
+    'Terminal': 'terminal',
+    'Textmate': 'textmate',
+    'Monokai': 'monokai',
+    'Twilight': 'twilight',
+    'Dracula': 'dracula',
+    'Github': 'github',
+    'Night Blue': 'tomorrow_night_blue'
+};
+
+$(document).ready(function (e) {
+    const program = localStorage.getItem("code");
+    const languageKey = localStorage.getItem("language") ? JSON.parse(localStorage.getItem("language")) : "Node JS";
+    const themeKey = localStorage.getItem("theme") ? JSON.parse(localStorage.getItem("theme")) : "Terminal";
+    const size = localStorage.getItem("fontSize") ? JSON.parse(localStorage.getItem("fontSize")) : "18";
+
+    let theme = themes[themeKey];
+    editor.setTheme(`ace/theme/${theme}`);
+    $("#theme").text(`${themeKey}`);
+
+    let language = langs[languageKey];
+    console.log(language);
+    editor.session.setMode(`ace/mode/${language}`);
+    $("#language").text(`${languageKey}`);
+
+    editor.session.setValue(JSON.parse(program), -1);
+
+    editor.setOptions({
+        autoScrollEditorIntoView: true,
+        copyWithEmptySelection: true,
+        enableBasicAutocompletion: true,
+        enableLiveAutocompletion: true,
+        enableSnippets: true,
+        fontSize: size + "px"
+    });
+
+    $("#font-size").text(`${size}`);
+});
 
 function editorInit() {
     // set theme
@@ -43,12 +86,14 @@ $(".dropdown-item.nameTheme").click(function (e) {
     editor.setTheme(`ace/theme/${theme}`);
     let name = $(this).text();
     $("#theme").text(`${name}`);
+    localStorage.setItem("theme", JSON.stringify(name));
 });
 
 $(".dropdown-item.lang").click(function (e) {
     let language = $(this).attr("id");
     let name = $(this).text();
     $("#language").text(`${name}`);
+    localStorage.setItem("language", JSON.stringify(name));
     handleLangChange(language);
 });
 
@@ -56,7 +101,7 @@ function handleLangChange(language) {
     let snippet;
     switch (language) {
         case 'javascript':
-            snippet = defaultCode;
+            snippet = 'console.log("Hello, World!");';
             break;
 
         case 'c_cpp':
@@ -77,6 +122,8 @@ function handleLangChange(language) {
 
     editor.setValue(snippet);
     editor.session.setMode(`ace/mode/${language}`);
+    const code = editor.session.getValue();
+    localStorage.setItem("code", JSON.stringify(code));
 }
 
 
@@ -115,20 +162,14 @@ resetBtn.click(function (e) {
 });
 
 function aceToJDoodle() {
-    let language = $("#language").text();
-    let langs = {
+    let lang = {
         'Node JS': 'nodejs',
         'C++': 'cpp',
         'Python': 'python3',
         'Java': 'java'
-    }
-    
-    let keys = Object.keys(langs);
-    for(let key of keys) {
-        if(language === key) {
-            return langs[key];
-        }
-    }   
+    };
+    let language = $("#language").text();
+    return lang[language];
 }
 
 function handleOutput(data) {
@@ -157,4 +198,13 @@ $(".dropdown-item.fs").click(function (e) {
         fontSize: fs + "px"
     });
     $("#font-size").text(`${fs}`);
+    localStorage.setItem("fontSize", JSON.stringify(fs));
 });
+
+editor.on("change", function () {
+    const code = editor.session.getValue();
+    localStorage.setItem("code", JSON.stringify(code));
+});
+
+
+
