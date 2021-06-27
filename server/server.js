9912fe68703f3b0b5bfac69a236147448cba7d30
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-06-27 20:23:21 +0530

    updates

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const request = require("request");
+const cors = require('cors');
 require('dotenv').config()
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 3000;
 app.post("/", (req, res) => {
     res.set({
