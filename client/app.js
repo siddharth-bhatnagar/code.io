deb1debda7fc6a7be75355143ec2bcce530fc3c6
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-06-29 22:52:44 +0530

    Loader added

--- client/app.js
+++ client/app.js
@@ -129,7 +129,7 @@ function handleLangChange(language) {
 
 async function compileIt(code, lang) {
 
-    let response = await fetch("https://code-editor-io.herokuapp.com/", {
+    let response = await fetch("", {
         method: "POST",
         headers: {
             "Content-Type": "text/plain"
